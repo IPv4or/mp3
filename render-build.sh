c6ecd095a6f9756449e5bc526b4d6762a6947671
IPv6ix <arpa.netease@gmail.com>
2025-08-27 20:47:52 -0600

    v2

--- render-build.sh
+++ render-build.sh
@@ -7,13 +7,3 @@ pip install -r requirements.txt
 
 # Install ffmpeg
 apt-get update && apt-get install -y ffmpeg
-
-# Create the directories on the persistent disk if they don't exist
-# The Python app also does this, but it's good practice for the build script to handle it.
-mkdir -p /data/downloads
-
-# Copy the initial cookies.txt from your repo to the persistent disk
-# This will only happen once when the disk is first created.
-if [ ! -f /data/cookies.txt ]; then
-  cp cookies.txt /data/cookies.txt
-fi
