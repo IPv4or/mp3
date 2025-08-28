2a5fb83f1a345009aa1d3bd8136509586a4bc76b
IPv6ix <arpa.netease@gmail.com>
2025-08-27 21:12:28 -0600

    v2

--- render-build.sh
+++ render-build.sh
@@ -2,9 +2,12 @@
 # exit on error
 set -e
 
+# Install python dependencies
 pip install -r requirements.txt
 
-# Clean, update, and install ffmpeg
+# Forcefully remove any existing lock files, clean, update, and then install ffmpeg.
+rm -f /var/lib/dpkg/lock-frontend
+rm -f /var/cache/apt/archives/lock
 apt-get clean
 apt-get update
 apt-get install -y --no-install-recommends ffmpeg
