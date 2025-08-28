23bdf89572141852897e6dfdec5bfe3bcd301041
IPv6ix <arpa.netease@gmail.com>
2025-08-27 21:08:57 -0600

    v2

--- render-build.sh
+++ render-build.sh
@@ -2,10 +2,9 @@
 # exit on error
 set -e
 
-# Install python dependencies
 pip install -r requirements.txt
 
-# Clean, update, and install ffmpeg. This is a more robust command.
+# Clean, update, and install ffmpeg
 apt-get clean
 apt-get update
 apt-get install -y --no-install-recommends ffmpeg
